b8e893a78f1d22592a4176e551b4dc73a653eb18
sydney <sydney.murray@yahoo.co.uk>
2023-05-11 19:16:05 +0100

    Added CarParkService class

---
+++ src/main/java/org/example/CarParkService.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class CarParkService {
+    private final int carSpaceCapacity = 30;
+    private final int largespaceCapacity = 10;
+    private final int motorcycleSpace = 5;
+
+    private int occupiedCarSpaces = 0;
+    private int occupiedLargeSpaces = 0;
+    private int occupiedMotorcycleSpaces = 0;
+    public void inService() {
+        while (true) {
+
+        }
+    }
+}
