117c91b83498ab0c2bbbdbe1b6687bbe722452fc
sydney <sydney.murray@yahoo.co.uk>
2023-05-12 10:13:40 +0100

    Added admitCar method to CarParkInformation class

--- src/main/java/org/example/CarParkInformation.java
+++ src/main/java/org/example/CarParkInformation.java
@@ -7,9 +7,28 @@ public class CarParkInformation {
     private final int largeSpaceCapacity = 10;
     private final int motorcycleSpaceCapacity = 5;
 
-    private int  totalCars = 0;
-    private int totalMotorcycles = 0;
-    private int totalVans = 0;
+    private int  totalCarSpaces = 0;
+    private int totalMotorcyclesSpaces = 0;
+    private int totalLargeSpaces = 0;
     private Scanner keyboardInput = new Scanner(System.in);
     private int selection;
+
+    public void admitACar(){
+        if (totalCarSpaces < carSpaceCapacity){
+            totalCarSpaces++;
+            pressEnterToContinue("\n    Access Granted.");
+            return;
+        }
+        if (totalLargeSpaces < largeSpaceCapacity){
+            totalLargeSpaces++;
+            pressEnterToContinue("\n    Access Granted.");
+            return;
+        }
+        pressEnterToContinue("\n    Car park is full: Access denied.");
+    }
+
+    private void pressEnterToContinue(String message){
+        System.out.println(message);
+        keyboardInput.nextLine();
+    }
 }
